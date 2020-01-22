adcd1ebd25abb3cce7484e087d83ec67378f34c7
Oktay Şenkan <oktay.senkan@gmail.com>
2020-01-22 10:13:48 +0300

    API request added.

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -3,10 +3,19 @@ import PropTypes from 'prop-types';
 
 import './Button.scss';
 
-export default function Button({ children }) {
-  return <div className="button">{children}</div>;
+export default function Button({ children, onClick }) {
+  return (
+    <button type="button" onClick={onClick} className="button">
+      {children}
+    </button>
+  );
 }
 
 Button.propTypes = {
   children: PropTypes.node.isRequired,
+  onClick: PropTypes.func,
+};
+
+Button.defaultProps = {
+  onClick: null,
 };
--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import Button from '../../components/Button';
 
 import './Home.scss';
 
 export default function Home() {
+  const history = useHistory();
+  const generateQuiz = async () => {
+    const response = await fetch(
+      'https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple',
+    );
+    const data = await response.json();
+    history.push('/question');
+    return data;
+  };
   return (
     <div className="container">
       <div className="logo" />
       <div className="slogan">A trivia game</div>
-      <Link to="/question">
-        <Button>GET STARTED</Button>
-      </Link>
+      <Button
+        onClick={() => {
+          generateQuiz();
+        }}
+      >
+        GET STARTED
+      </Button>
     </div>
   );
 }
